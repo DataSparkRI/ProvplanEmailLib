5818dd7b8f16f744c61ea6818ed863cfa1505f30
Angel Medrano <asmedrano@gmail.com>
2012-11-02 17:01:42 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,16 @@ try:
 except ImportError:
     from distutils.core import setup
 
-    config = {
+config = {
         'description': '',
         'author': 'Jesse Ellis',
-        'url': 'https://github.com/ProvidencePlan/provplan-email-lib',
-        'download_url': 'https://github.com/ProvidencePlan/provplan-email-lib/zipball/master',
+        'url': 'https://github.com/ProvidencePlan/ProvplanEmailLib',
+        'download_url': 'https://github.com/ProvidencePlan/ProvplanEmailLib/zipball/master',
+        'install_requires': [],
+        'packages': ['provplan_email_lib'],
         'author_email': 'jellis@provplan.org',
         'version': '0.1.0',
         'name': 'provplan_email_lib'
-        }
+}
 
-    setup(**config)
+setup(**config)
